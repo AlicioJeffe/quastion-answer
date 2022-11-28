496bbb9462916be1e8006667525bee12f173e37e
Alicio Jefferson <alicio.vicente@officegest.com>
2022-11-28 16:36:11 +0100

    first update

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.get('/', function (req, res) {
+    var lang = "Javascript";
+    res.render('home', {
+        lang, showMessage: false, products: [
+            { name: 'HP Computer', price: 2000 },
+            { name: 'Iphone 14 Pro Max', price: 94000 },
+            { name: 'Wi-fi Repeater with two antenna', price: 10500 },
+        ]
+    })
+});
+
+app.get('/ask', (req, res) => {
+    res.render('ask_question')
+})
+
+app.post('/sendForm', ((req, res) => {
+
+    res.end("Form sent. Data : Title: " + req.body['title'] + " Description: " + req.body.description)
+}))
+
+app.listen(8000, () => {
+    console.log('server running')
+})
